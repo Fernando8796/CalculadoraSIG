653fa474c14b1d1b7515b855b1ac75f6510697fa
José Fernando <jf8065572@gmail.com>
2024-05-01 15:16:15 -0300

    Add: Fazer operações com os 4 operadores

--- Main.java
+++ Main.java
@@ -17,8 +17,60 @@ public class Main {
         System.out.print("> ");
         String operador = scanner.nextLine();
 
-        System.out.println("Voce digitou: "+operador);
-        
+        switch(operador) {
+            //Caso soma
+            case "+":
+                System.out.print("Primeiro número: ");
+                int num1 = scanner.nextInt();
 
+                System.out.print("Segundo número: ");
+                int num2 = scanner.nextInt();
+
+                int soma = num1 + num2;
+
+                System.out.println("A soma é "+soma);
+            break;
+            //Caso subtração
+            case "-":
+                System.out.print("Primeiro número: ");
+                int num3 = scanner.nextInt();
+
+                System.out.print("Segundo número: ");
+                int num4 = scanner.nextInt();
+
+                int subtracao = num3 - num4;
+
+                System.out.println("A subtração é "+subtracao);
+            break;
+            //Caso multiplicação
+            case "*":
+                System.out.print("Primeiro número: ");
+                int num5 = scanner.nextInt();
+
+                System.out.print("Segundo número: ");
+                int num6 = scanner.nextInt();
+
+                int multiplicacao = num5 * num6;
+
+                System.out.println("A multiplicacão é "+multiplicacao);
+            break;
+            //Caso subtração
+            case "/":
+                System.out.print("Primeiro número: ");
+                int num7 = scanner.nextInt();
+
+                System.out.print("Segundo número: ");
+                int num8 = scanner.nextInt();
+
+                int divisao = num7 / num8;
+
+                System.out.println("A divisão é "+divisao);
+            break;
+            //Se não for nenhum dos operadores disponíveis
+            default:
+                System.out.println("Operador não encontrado");
+        }
+
+        scanner.close();
     }
 }
