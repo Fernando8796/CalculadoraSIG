85765bc62fd2d4ab10a8e7bb53d69e924a69d609
José Fernando <jf8065572@gmail.com>
2024-05-01 13:50:45 -0300

    Add: Printando os 4 operadores básicos

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        //Criando uma instância de Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //Printando os Operadores disponíveis
+        System.out.println("Escolha um operador: ");
+        System.out.println("[+] Soma");
+        System.out.println("[-] Subtração");
+        System.out.println("[*] Multiplicação");
+        System.out.println("[/] Divisão");
+
+        //Pegando o operador com o usuário
+        System.out.print("> ");
+        String operador = scanner.nextLine();
+
+        System.out.println("Voce digitou: "+operador);
         
+
     }
 }
